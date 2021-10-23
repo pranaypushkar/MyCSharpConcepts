3d4606d1e80869b90c6f3d30c8bee8d9d861fc1e
PRANAY PUSHKAR <pranaypushkar92@gmail.com>
2021-10-23 15:18:11 +0000

    set matrix to 1

--- main.cs
+++ main.cs
@@ -8,14 +8,56 @@ class Program
 {
   public static void Main (string[] args) 
   {
-    int number = 5;
-    // Top Edge case
-    if(number < 0)
+    int[,] input = new int[,]
     {
-        Console.WriteLine("Invalid Number");
+      {0,0,0,0},
+      {0,0,0,1},
+      {0,0,0,0}
+    };
+int rowIndex = 0;
+int columnIndex = 0;
+bool isFound = false;
+  for (int i = 0; i < input.GetLength(0); i++) 
+    {
+      for (int j = 0; j < input.GetLength(1); j++) 
+      { 
+        if(input[i,j] == 1)
+        {
+          rowIndex = i;
+          columnIndex = j;
+          isFound = true;
+          break;
+        }
+        if(isFound)
+        {
+          break;
+        }
+      }
+   }
+
+
+  for (int i = 0; i < input.GetLength(0); i++) 
+    {
+      for (int j = 0; j < input.GetLength(1); j++) 
+      {
+       if(j == columnIndex)
+         input[i, j] = 1;
+
+        if(i == rowIndex)
+         input[i, j] = 1;
+      }
     }
 
-    Factorial factorial = new Factorial();
-    Console.WriteLine(factorial.Calculate(number));
+
+    Console.WriteLine("Printing Matrix: ");
+    for (int i = 0; i < input.GetLength(0); i++) 
+    {
+      for (int j = 0; j < input.GetLength(1); j++) 
+      {
+         Console.Write(input[i, j] + "\t");
+      }
+      Console.WriteLine();
+    }
+    Console.ReadLine();
   }
 }
